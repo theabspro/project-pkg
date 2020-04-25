52038ba0a55b90324976586473ad717de2f683d3
Abdul <abdul@uitouxsolutions.com>
2020-04-25 22:07:01 +0530

    Update 2020_04_24_121737_documents_c.php

--- src/database/migrations/2020_04_24_121737_documents_c.php
+++ src/database/migrations/2020_04_24_121737_documents_c.php
@@ -1,47 +1,46 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class DocumentsC extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up() {
-        if (!Schema::hasTable('documents')) {
-            Schema::create('documents', function (Blueprint $table) {
-                $table->increments('id');
-                $table->unsignedInteger('project_requirement_id');
-                $table->string('name',255);
-                $table->unsignedInteger('type_id');
-                $table->text('value');
-                $table->unsignedInteger('created_by_id')->nullable();
-                $table->unsignedInteger('updated_by_id')->nullable();
-                $table->unsignedInteger('deleted_by_id')->nullable();
-                $table->timestamps();
-                $table->softDeletes();
+class DocumentsC extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		if (!Schema::hasTable('documents')) {
+			Schema::create('documents', function (Blueprint $table) {
+				$table->increments('id');
+				$table->unsignedInteger('project_requirement_id');
+				$table->string('name', 255);
+				$table->unsignedInteger('type_id');
+				$table->text('value');
+				$table->unsignedInteger('created_by_id')->nullable();
+				$table->unsignedInteger('updated_by_id')->nullable();
+				$table->unsignedInteger('deleted_by_id')->nullable();
+				$table->timestamps();
+				$table->softDeletes();
 
-                $table->foreign('project_requirement_id')->references('id')->on('project_versions')->onDelete('CASCADE')->onUpdate('cascade');
-                $table->foreign('type_id')->references('id')->on('configs')->onDelete('CASCADE')->onUpdate('cascade');
-                $table->foreign('created_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
-                $table->foreign('updated_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
-                $table->foreign('deleted_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+				$table->foreign('project_requirement_id')->references('id')->on('project_versions')->onDelete('CASCADE')->onUpdate('cascade');
+				$table->foreign('type_id')->references('id')->on('configs')->onDelete('CASCADE')->onUpdate('cascade');
+				$table->foreign('created_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+				$table->foreign('updated_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
+				$table->foreign('deleted_by_id')->references('id')->on('users')->onDelete('SET NULL')->onUpdate('cascade');
 
-                $table->unique(["project_requirement_id", "name","type_id"],'douments_unique');
-            });
-        }
-    }
+				$table->unique(["project_requirement_id", "name", "type_id"], 'doc_unique');
+			});
+		}
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down() {
-        Schema::dropIfExists('documents');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('documents');
+	}
 }
